f9e5a5585109b2e720678b2f064522a12369ad46
lucasmaestri <lucas.maestri@icloud.com>
2022-03-13 21:46:45 -0300

    13.03 ios

---
+++ etapas.js
@@ -0,0 +1,44 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numero: 5,
+        candidatos: [
+            {
+                numero: "12345",
+                nome: "Renato Silva",
+                partido: "QWE",
+                fotos: [{scr:"img01.png", legenda:"Vereador"}],
+            },
+            {
+                numero: "54321",
+                nome: "João Barbosa",
+                partido: "EWQ",
+                fotos: [{scr:"img02.png", legenda:"Vereador"}],
+            },
+        ],
+    },
+    {
+        titulo: "PREFEITO",
+        numero: 2,
+        candidatos: [
+            {
+                numero: "99",
+                nome: "Fagner Fagundes",
+                partido: "QWE",
+                fotos: [
+                    {scr:"img03.png", legenda:"Prefeito"},
+                    {scr:"img03_1.jpeg", legenda:"Vice-Prefeito", small: true},
+                ],
+            },
+            {
+                numero: "77",
+                nome: "Carlos Suarez",
+                partido: "QWE",
+                fotos: [
+                    {scr:"img04.jpeg", legenda:"Prefeito"},
+                    {scr:"img04_1.png", legenda:"Vice-Prefeito", small: true},
+                ],
+            },
+        ],
+    },
+]
